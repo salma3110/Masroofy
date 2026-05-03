bee585afc67d96b9603c68bd632757e9ecd35ae3
JanaEzz <ezzeldenjana@gmail.com>
2026-05-03 22:34:55 +0300

    Now all apps can see and communicate with eachother

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import User
+from django.utils import timezone
+from datetime import timedelta
+from django.contrib.auth.hashers import make_password, check_password
 
-# Create your views here.
+def setup(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        pin = request.POST.get("pin")
+
+        User.objects.all().delete()  # keep only one user
+        hashed_pin = make_password(pin)
+        User.objects.create(name=name, pin=hashed_pin)
+
+        return redirect('login')
+
+    return render(request, 'authentication/setup.html')
+
+
+def login(request):
+    user = User.objects.first()
+
+    if not user:
+        return redirect('setup')
+    
+    if user.is_locked:
+        if user.lock_time and timezone.now() > user.lock_time + timedelta(seconds=30):
+            user.is_locked = False
+            user.failed_attempts = 0
+            user.save()
+        else:
+            return redirect('lockout')
+
+    if request.method == "POST":
+        pin = request.POST.get("pin")
+
+        if check_password(pin, user.pin):
+            user.failed_attempts = 0
+            user.save()
+            return redirect('setup')  # MAFROOD A DIRECT LEL DASHBOARD BTA3T HALA 
+
+        else:
+             user.failed_attempts += 1
+             if user.failed_attempts >= 3:
+                user.is_locked = True
+                user.lock_time = timezone.now()
+                user.save()
+                return redirect('lockout')
+             user.save()
+             return render(request, 'authentication/login.html', {'error': 'Incorrect PIN. Try again.'})
+            
+    return render(request, 'authentication/login.html')         
+def lockout(request):
+    return render(request, 'authentication/lockout.html')
--- expenses/urls.py
+++ expenses/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.add_expense),  
+    #path('', views.add_expense),  
     path('add/', views.add_expense, name='add'),
     path('history/', views.history, name='history'),
     path('delete/<int:id>/', views.delete_expense, name='delete'),
