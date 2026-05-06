9b5ae0f7b26abb2d2944b384563310c820de88bc
Salma Salah <salma.salahh9@gmail.com>
2026-05-06 12:38:01 +0300

    conflict done

--- BudgetCycle/urls.py
+++ BudgetCycle/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'budget_cycle'
+
 urlpatterns = [
     path('setup/', views.setup, name='setup'),
     path('change-budget/', views.change_budget, name='change_budget'),
